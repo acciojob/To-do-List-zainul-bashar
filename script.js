adea1fc8166275260ac1d78b0190903b2d0aef71
zainul-bashar <120213051+zainul-bashar@users.noreply.github.com>
2023-02-15 14:32:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+var btn=document.getElementById("addTodoBtn");
+var input=document.getElementById("newTodoInput");
+var ol=document.getElementById("todoList");
+btn.addEventListener("click",addList);
+function addList(){
+	if(input.value){
+		var li=document.createElement("li");
+		li.textContent=input.value;
+		ol.appendChild(li);
+		input.value="";
+	}
+	
+}
 
